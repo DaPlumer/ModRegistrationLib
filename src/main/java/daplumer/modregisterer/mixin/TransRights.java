0ec764ed012a179b861819066f5efcf16b141436
DaPlumer <daplumer271@gmail.com>
2025-04-19 22:13:13 -0400

    Initial commit

---
+++ src/main/java/daplumer/modregisterer/mixin/TransRights.java
@@ -0,0 +1,19 @@
+package daplumer.modregisterer.mixin;
+
+import net.minecraft.client.gui.screen.SplashTextRenderer;
+import net.minecraft.client.resource.SplashTextResourceSupplier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * :3
+ */
+@Mixin(SplashTextResourceSupplier.class)
+public abstract class TransRights {
+    @Inject(method = "get", at = @At("HEAD"),cancellable = true)
+    private void injected(CallbackInfoReturnable<SplashTextRenderer> cir){
+        cir.setReturnValue(new SplashTextRenderer("Trans Rights!"));
+    }
+}
